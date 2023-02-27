0a108a11beeae563c379bfff27ab64a587289c53
haroldirvingpumapolo <pumapoloharold123@gmail.com>
2023-02-27 17:45:40 -0500

    formulario commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Text} from "@chakra-ui/react"
+const App:React.FC = () => {
+  return (
+    <div>
+      <Text color={"primary.500"}>hola</Text>
+    </div>
+  )
+};
+
+export default App;
